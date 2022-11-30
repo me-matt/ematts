5f912ba50a6f7eadc50e8785dc027fe16fa8bb97
Matt Waldron <12972009+me-matt@users.noreply.github.com>
2022-11-30 16:31:16 +0000

    chore: initial setup

--- init.el
+++ init.el
@@ -1,3 +1,7 @@
+
+(defvar ematts/default-font-size 175)
+(defvar ematts/projects-path "~/Documents/src")
+
 (setq inhibit-startup-message t)
 
 (scroll-bar-mode -1)      ; Disable visual scrollbar
@@ -5,12 +9,20 @@
 (tooltip-mode -1)         ; Disable tooltips
 (set-fringe-mode 10)      ; Give some breathing room
 
+
 (menu-bar-mode -1)        ; Disable the menu bar
+
+(indent-tabs-mode -1)
+
+;(resize-mini-windows t)
+
+(global-whitespace-mode 1)
 
 ;; Set up the visible bell
 (setq visible-bell t)
 
-(set-face-attribute 'default nil :font "Monaco" :height 150)
+(set-face-attribute 'default nil :font "Monaco" :height ematts/default-font-size)
+(set-face-attribute 'whitespace-indentation nil :background nil)
 
 (load-theme 'wombat)
 
@@ -20,9 +32,9 @@
 (require 'package)
 
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("melpa-stable" . "https://stable.melpa.org/packages/")
-			 ("org" . "https://orgmode.org/elpa/")
-			 ("elpa" . "https://elpa.gnu.org/packages/")))
+                         ("melpa-stable" . "https://stable.melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
 
 (package-initialize)
 (unless package-archive-contents
@@ -35,13 +47,24 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+;; Disable line numbers for some modes
+(dolist (mode '(org
+                shell-mode-hook
+               ; eshell-mode-hook
+                term-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
 (use-package command-log-mode)
+(global-command-log-mode)
 
 (use-package ivy
   :diminish
   :bind (("C-s" . swiper)
          :map ivy-minibuffer-map
-         ("TAB" . ivy-alt-done)	
+         ("TAB" . ivy-alt-done)
          ("C-l" . ivy-alt-done)
          ("C-j" . ivy-next-line)
          ("C-k" . ivy-previous-line)
@@ -55,7 +78,42 @@
   :config
   (ivy-mode 1))
 
-(use-package counsel)
+(use-package doom-modeline
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
+
+(use-package doom-themes
+  :init (load-theme 'doom-Iosvkem t))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
+
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+         ("C-x b" . counsel-ibuffer)
+         ("C-x C-f" . counsel-find-file)
+         :map minibuffer-local-map
+         ("C-r" . 'counsel-minibuffer-history)))
+
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
 
 (use-package swiper)
 
@@ -69,3 +127,91 @@
 (setq doom-modeline-github 1)
 
 (use-package all-the-icons)
+
+(use-package general
+  :config
+  (general-create-definer ematts/leader-keys
+			  :keymaps '(normal insert visual emacs)
+			  :prefix "SPC"
+			  :global-prefix "C-SPC")
+
+  (ematts/leader-keys
+    "t"  '(:ignore t :which-key "toggles")
+    "tt" '(counsel-load-theme :which-key "choose theme")))
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
+(use-package hydra)
+
+(defhydra hydra-text-scale (:timeout 4)
+  "scale text"
+  ("j" text-scale-increase "in")
+  ("k" text-scale-decrease "out")
+  ("f" nil "finished" :exit t))
+
+(ematts/leader-keys
+  "ts" '(hydra-text-scale/body :which-key "scale text"))
+
+(defhydra hydra-window-width (:timeout 4)
+  "window width"
+  ("j" evil-window-increase-width "in")
+  ("k" evil-window-decrease-width "out")
+  ("f" nil "finished" :exit t))
+
+(ematts/leader-keys
+  "ww" '(hydra-window-width/body :which-key "window width"))
+
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom (projectile-completion-system 'ivy)
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  (when (file-directory-p "~/Documents/src")
+    (setq projectile-project-search-path '("~/Documents/src")))
+ 
+  (setq projectile-switch-project-action 'projectile-dired))
+
+(use-package counsel-projectile
+  :config (counsel-projectile-mode))
+
+(use-package magit
+  :custom
+  (magit-display-buffer-function 'magit-display-buffer-same-window-except-diff-v1))
+
+(use-package forge)
+
+(use-package yaml-mode)
+
+(add-hook 'yaml-mode-hook
+	  '(lambda ()
+	     (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
+
+(add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
+
+(use-package treemacs)
+(use-package treemacs-evil)
+(use-package treemacs-projectile)
+
